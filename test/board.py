2dd7dbaf622b5c3b81cb1533ae2943db00d12b8f
mehrdad <mehrdadkarrabi1997@yahoo.com>
2021-05-24 23:49:15 +0430

    board

--- test/board.py
+++ test/board.py
@@ -5,7 +5,11 @@ class board:
                     '1': ' ' , '2': ' ' , '3': ' ' }
 
     def printBoard(self):
-        print(self.theBoard)
+        print(self.theBoard['7'] + '|' + self.theBoard['8'] + '|' + self.theBoard['9'])
+        print('-+-+-')
+        print(self.theBoard['4'] + '|' + self.theBoard['5'] + '|' + self.theBoard['6'])
+        print('-+-+-')
+        print(self.theBoard['1'] + '|' + self.theBoard['2'] + '|' + self.theBoard['3'])
 
     def resetBoard(self):
         self.theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,

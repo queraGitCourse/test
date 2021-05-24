597b8549614cb02961dbe6f3a97a21d2903e20b4
mehrdad <mehrdadkarrabi1997@yahoo.com>
2021-05-24 23:40:18 +0430

    first commit

---
+++ test/main.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    continue_playing = True
+
+    print("Welcome to Tic Tac Toe!!!")
+    player1 = input("Enter the first player's name: ")
+    player2 = input("Enter the second player's name: ")
+    print("Nice! let's start ...")
+    
+    while continue_playing:
+        pass
+        # TODO
